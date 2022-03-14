53604087c9629f80d9a0d5f59eebdbeaf9956b64
sondo <son.do@treasure-data.com>
2022-03-14 16:04:17 +0700

    common colors

--- consentdemo/script.js
+++ consentdemo/script.js
@@ -5,6 +5,7 @@
   var denyButtonColorContaqiner = doc.querySelector('.deny');
   var confirmationButtonColorContaqiner = doc.querySelector('.confirmation');
   var layoutContainer = doc.querySelector('.layout');
+  var commonColors = doc.querySelector('.common');
 
 
   sizingContainer.addEventListener('click', function (evt) {
@@ -14,6 +15,31 @@
     var percentage = target.textContent;
 
     Treasure.CookieControl.banner.width = percentage;
+  });
+
+  commonColors.addEventListener('change', function (evt) {
+    var target = evt.target;
+    var name = target.name;
+
+    var currentColorScheme = {};
+
+    if (Treasure.CookieControl.banner) {
+       currentColorScheme = { ...Treasure.CookieControl.banner.colorScheme };
+    } else if (Treasure.CookieControl.preferenceModal) {
+       currentColorScheme = { ...Treasure.CookieControl.preferenceModal.colorScheme };
+    }
+    if (name === 'bg') {
+      currentColorScheme['backgroundColor']= target.value;
+    } else if (name === 'buttonColor') {
+      currentColorScheme['buttonColor'] = target.value;
+    }
+
+    if (Treasure.CookieControl.banner) {
+      Treasure.CookieControl.banner.colorScheme = currentColorScheme;
+    }
+    if (Treasure.CookieControl.preferenceModal) {
+      Treasure.CookieControl.preferenceModal.colorScheme = currentColorScheme;
+    }
   });
 
   acceptButtonColorContaqiner.addEventListener('change', function (evt) {
