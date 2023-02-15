d0102c2a6e99c6f9dd2de9556bc68e5e90ec894d
son.do <son.do@treasure-data.com>
2023-02-16 06:46:05 +0700

    multiple

--- uc/multiple.js
+++ uc/multiple.js
@@ -2,8 +2,8 @@
   var saveBtn = document.querySelector(".save-btn");
   var uncheckAll = document.querySelector("#uncheckall");
 
-  var form1 = document.querySelector("uc-form-1");
-  var form2 = document.querySelector("uc-form-2");
+  var form1 = document.querySelector("#uc-form-1");
+  var form2 = document.querySelector("#uc-form-2");
 
   if (saveBtn) {
     saveBtn.addEventListener("click", () => {
