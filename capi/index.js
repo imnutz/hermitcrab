a06d7f8e129a350e82d01832da809e7d7e992b57
Son Do <son.do@treasure-data.com>
2024-09-13 10:47:49 +0700

    capi

--- capi/index.js
+++ capi/index.js
@@ -1,4 +1,5 @@
 !(function () {
+  var cookieOptions = { secure: true };
   function addUrlParam(name, value) {
     var currentUrl = new URL(window.location.href);
 
@@ -26,7 +27,7 @@
   }
 
   function set_ga_Cookie() {
-    Cookies.set("_ga", "ga_value", { secure: true });
+    Cookies.set("_ga", "ga_value", cookieOptions);
   }
 
   function get_ga_Cookie() {
@@ -34,14 +35,14 @@
   }
 
   function set_gcl_aw_Cookie() {
-    Cookies.set("_gcl_aw", "gclaw_value");
+    Cookies.set("_gcl_aw", "gclaw_value", cookieOptions);
   }
   function get_gcl_aw_Cookie() {
     alert(td.getGoogleConversionLinkerCookie());
   }
 
   function set_fbp_Cookie() {
-    Cookies.set("_fbp", "fbp_value");
+    Cookies.set("_fbp", "fbp_value", cookieOptions);
   }
 
   function get_fbp_Cookie() {
@@ -49,7 +50,7 @@
   }
 
   function set_fbc_Cookie() {
-    Cookies.set("_fbc", "fbc_value");
+    Cookies.set("_fbc", "fbc_value", cookieOptions);
   }
 
   function get_fbc_Cookie() {
@@ -57,7 +58,7 @@
   }
 
   function set_yjsu_yjad_Cookie() {
-    Cookies.set("_yjsu_yjad", "yjsuyjad_value");
+    Cookies.set("_yjsu_yjad", "yjsuyjad_value", cookieOptions);
   }
 
   function get_yjsu_yjad_Cookie() {
@@ -65,7 +66,7 @@
   }
 
   function set_ycl_yjad_Cookie() {
-    Cookies.set("_ycl_yjad", "yclyjad_value");
+    Cookies.set("_ycl_yjad", "yclyjad_value", cookieOptions);
   }
 
   function get_ycl_yjad_Cookie() {
@@ -73,7 +74,7 @@
   }
 
   function set_yjr_yjad_Cookie() {
-    Cookies.set("_yjr_yjad", "yjryjad_value");
+    Cookies.set("_yjr_yjad", "yjryjad_value", cookieOptions);
   }
 
   function get_yjr_yjad_Cookie() {
@@ -81,7 +82,7 @@
   }
 
   function set_twclid_Cookie() {
-    Cookies.set("_twclid", "twclid_value");
+    Cookies.set("_twclid", "twclid_value", cookieOptions);
   }
 
   function get_twclid_Cookie() {
@@ -89,7 +90,7 @@
   }
 
   function set_lt_cid_Cookie() {
-    Cookies.set("__lt__cid", "ltcid_value");
+    Cookies.set("__lt__cid", "ltcid_value", cookieOptions);
   }
 
   function get_lt_cid_Cookie() {
@@ -100,50 +101,51 @@
     return document.querySelector(`#${id}`);
   }
 
+  function addClickEvent(element, handler) {
+    element.addEventListener("click", handler);
+  }
+
   function setup() {
     // add para actions
-    getElement("para_gclid").addEventListener("click", addGoogleClidParam);
-    getElement("para_fbclid").addEventListener("click", addFacebookClidParam);
-    getElement("para_yclid").addEventListener("click", addYahoo_yclid_Param);
-    getElement("para_yjryjad").addEventListener(
-      "click",
-      addYahoo_yjr_yjad_Param,
-    );
+    addClickEvent(getElement("para_gclid"), addGoogleClidParam);
+    addClickEvent(getElement("para_fbclid"), addFacebookClidParam);
+    addClickEvent(getElement("para_yclid"), addYahoo_yclid_Param);
+    addClickEvent(getElement("para_yjryjad"), addYahoo_yjr_yjad_Param);
 
     // add cookie actions
-    getElement("c_ga").addEventListener("click", set_ga_Cookie);
-    getElement("c_gclaw").addEventListener("click", set_gcl_aw_Cookie);
-    getElement("c_fbp").addEventListener("click", set_fbp_Cookie);
-    getElement("c_fbc").addEventListener("click", set_fbc_Cookie);
-    getElement("c_yjsuyjad").addEventListener("click", set_yjsu_yjad_Cookie);
-    getElement("c_yclyjad").addEventListener("click", set_ycl_yjad_Cookie);
-    getElement("c_yjryjad").addEventListener("click", set_yjr_yjad_Cookie);
-    getElement("c_twclid").addEventListener("click", set_twclid_Cookie);
-    getElement("c_ltcid").addEventListener("click", set_lt_cid_Cookie);
+    addClickEvent(getElement("c_ga"), set_ga_Cookie);
+    addClickEvent(getElement("c_gclaw"), set_gcl_aw_Cookie);
+    addClickEvent(getElement("c_fbp"), set_fbp_Cookie);
+    addClickEvent(getElement("c_fbc"), set_fbc_Cookie);
+    addClickEvent(getElement("c_yjsuyjad"), set_yjsu_yjad_Cookie);
+    addClickEvent(getElement("c_yclyjad"), set_ycl_yjad_Cookie);
+    addClickEvent(getElement("c_yjryjad"), set_yjr_yjad_Cookie);
+    addClickEvent(getElement("c_twclid"), set_twclid_Cookie);
+    addClickEvent(getElement("c_ltcid"), set_lt_cid_Cookie);
 
-    getElement("get_ga").addEventListener("click", get_ga_Cookie);
-    getElement("get_gclaw").addEventListener("click", get_gcl_aw_Cookie);
-    getElement("get_fbp").addEventListener("click", get_fbp_Cookie);
-    getElement("get_fbc").addEventListener("click", get_fbc_Cookie);
-    getElement("get_ycapi").addEventListener("click", get_yjsu_yjad_Cookie);
-    getElement("get_yclyjad").addEventListener("click", get_ycl_yjad_Cookie);
-    getElement("get_yjryjad").addEventListener("click", get_yjr_yjad_Cookie);
-    getElement("get_twclid").addEventListener("click", get_twclid_Cookie);
-    getElement("get_ltcid").addEventListener("click", get_lt_cid_Cookie);
+    addClickEvent(getElement("get_ga"), get_ga_Cookie);
+    addClickEvent(getElement("get_gclaw"), get_gcl_aw_Cookie);
+    addClickEvent(getElement("get_fbp"), get_fbp_Cookie);
+    addClickEvent(getElement("get_fbc"), get_fbc_Cookie);
+    addClickEvent(getElement("get_ycapi"), get_yjsu_yjad_Cookie);
+    addClickEvent(getElement("get_yclyjad"), get_ycl_yjad_Cookie);
+    addClickEvent(getElement("get_yjryjad"), get_yjr_yjad_Cookie);
+    addClickEvent(getElement("get_twclid"), get_twclid_Cookie);
+    addClickEvent(getElement("get_ltcid"), get_lt_cid_Cookie);
 
-    getElement("get_p_gclid").addEventListener("click", function () {
+    addClickEvent(getElement("get_p_gclid"), function () {
       alert(td.getGoogleClickIDParam());
     });
 
-    getElement("get_p_fbclid").addEventListener("click", function () {
+    addClickEvent(getElement("get_p_fbclid"), function () {
       alert(td.getFacebookClickIDParam());
     });
 
-    getElement("get_p_yclid").addEventListener("click", function () {
+    addClickEvent(getElement("get_p_yclid"), function () {
       alert(td.getYahooClickID_yclid_Param());
     });
 
-    getElement("get_p_yjryjad").addEventListener("click", function () {
+    addClickEvent(getElement("get_p_yjryjad"), function () {
       alert(td.getYahooClickID_yjr_yjad_Param());
     });
   }
