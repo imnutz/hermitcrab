6763aa063934afc7328bc3b651fa27a32dc4ab47
Son Do <son.do@treasure-data.com>
2024-11-08 13:26:35 +0700

    update build

--- build.js
+++ build.js
@@ -1,101 +1,96 @@
 #!/usr/bin/env node
 var glob = require("glob");
-var fs = require('fs')
-var dotenv = require('dotenv');
+var fs = require("fs");
+var dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
 var fakeInfo = "xxxxxxxxxx";
 
-function replaceTdInfo (path, database, host, apiKey, segmentToken, profileToken) {
-  database = database || fakeInfo
-  host = host || fakeInfo
-  apiKey = apiKey || fakeInfo
-  segmentToken = segmentToken || fakeInfo
+function replaceTdInfo(
+  path,
+  database,
+  host,
+  apiKey,
+  segmentToken,
+  profileToken,
+) {
+  database = database || fakeInfo;
+  host = host || fakeInfo;
+  apiKey = apiKey || fakeInfo;
+  segmentToken = segmentToken || fakeInfo;
 
-  var files = glob.sync(path)
-  files.forEach(fileName => {
-    var data = fs.readFileSync(fileName, 'utf-8')
+  var files = glob.sync(path);
+  files.forEach((fileName) => {
+    var data = fs.readFileSync(fileName, "utf-8");
     var replacedData = data
-      .replace(/database:.*'/g, `database: '${database}'`)
-      .replace(/host:.*'/g, `host: '${host}'`)
-      .replace(/writeKey:.*'/g, `writeKey: '${apiKey}'`)
+      .replace(/database:.*[',"]/g, `database: '${database}'`)
+      .replace(/host:.*[',"]/g, `host: '${host}'`)
+      .replace(/writeKey:.*[',"]/g, `writeKey: '${apiKey}'`)
       .replace(/audienceToken:.*\[.*\]/g, `audienceToken: ['${segmentToken}']`)
-      .replace(/token:.*\[.*\]/g, `token: '${profileToken}'`)
+      .replace(/token:.*\[.*\]/g, `token: '${profileToken}'`);
 
-    fs.writeFileSync(fileName, replacedData, 'utf-8')
-  })
+    fs.writeFileSync(fileName, replacedData, "utf-8");
+  });
 }
 
 function fastApiKey() {
-  var data = fs.readFileSync('./audiences/test_audiences.html', 'utf-8')
-  var replacedData = data
-    .replace('TD_MASTER_API_KEY', process.env.td_prod_api_key)
+  var data = fs.readFileSync("./audiences/test_audiences.html", "utf-8");
+  var replacedData = data.replace(
+    "TD_MASTER_API_KEY",
+    process.env.td_prod_api_key,
+  );
 
-  fs.writeFileSync('./audiences/test_audiences.html', replacedData, 'utf-8')
+  fs.writeFileSync("./audiences/test_audiences.html", replacedData, "utf-8");
 }
 
-let {
-  database,
-  tdHost,
-  apiKey,
-  segmentToken,
-  c360Host,
-  profileToken
-} = process.env
+let { database, tdHost, apiKey, segmentToken, c360Host, profileToken } =
+  process.env;
+
+replaceTdInfo("./*.html", database, c360Host, apiKey, segmentToken);
 
 replaceTdInfo(
-  './*.html',
-  database,
-  c360Host,
-  apiKey,
-  segmentToken
-);
-
-replaceTdInfo(
-  './c360/*.html',
+  "./c360/*.html",
   database,
   c360Host,
   apiKey,
   segmentToken,
-  profileToken
+  profileToken,
 );
 replaceTdInfo(
-  './cors/*.html',
+  "./cors/*.html",
   database,
   c360Host,
   apiKey,
   segmentToken,
-  profileToken
+  profileToken,
 );
 replaceTdInfo(
-  './cors2/*.html',
+  "./cors2/*.html",
   database,
   c360Host,
   apiKey,
   segmentToken,
-  profileToken
+  profileToken,
 );
 
-
 replaceTdInfo(
-  './capi/*.html',
+  "./capi/*.html",
   database,
   c360Host,
   apiKey,
   segmentToken,
-  profileToken
+  profileToken,
 );
-
 
 let { databaseAp03, tdHostAp03, apiKeyAp03, segmentTokenAp03 } = process.env;
 
 replaceTdInfo(
-  './ap03/*.html',
+  "./ap03/*.html",
   databaseAp03,
   tdHostAp03,
   apiKeyAp03,
-  segmentTokenAp03
+  segmentTokenAp03,
 );
 
-fastApiKey()
+fastApiKey();
