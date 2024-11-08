e3efba1dc6187af034f5af494606d14812952faf
Son Do <son.do@treasure-data.com>
2024-11-08 09:24:31 +0700

    more capi

--- capi/index.js
+++ capi/index.js
@@ -1,4 +1,5 @@
 !(function () {
+  // Utils ===============================================
   var cookieOptions = { secure: true };
   function addUrlParam(name, value) {
     var currentUrl = new URL(window.location.href);
@@ -10,93 +11,6 @@
     window.location.href = currentUrl.href;
   }
 
-  function addGoogleClidParam() {
-    addUrlParam("gclid", "gclid_value");
-  }
-
-  function addFacebookClidParam() {
-    addUrlParam("fbclid", "fbclid_value");
-  }
-
-  function addYahoo_yclid_Param() {
-    addUrlParam("yclid", "yclid_value");
-  }
-
-  function addYahoo_yjr_yjad_Param() {
-    addUrlParam("yjr_yjad", "yjr_yjad_value");
-  }
-
-  function set_ga_Cookie() {
-    Cookies.set("_ga", "ga_value", cookieOptions);
-  }
-
-  function get_ga_Cookie() {
-    alert(td.getGoogleAnalyticsCookie());
-  }
-
-  function set_gcl_aw_Cookie() {
-    Cookies.set("_gcl_aw", "gclaw_value", cookieOptions);
-  }
-  function get_gcl_aw_Cookie() {
-    alert(td.getGoogleConversionLinkerCookie());
-  }
-
-  function set_fbp_Cookie() {
-    Cookies.set("_fbp", "fbp_value", cookieOptions);
-  }
-
-  function get_fbp_Cookie() {
-    alert(td.getFacebook1stCookie());
-  }
-
-  function set_fbc_Cookie() {
-    Cookies.set("_fbc", "fbc_value", cookieOptions);
-  }
-
-  function get_fbc_Cookie() {
-    alert(td.getFacebookClickIDCookie());
-  }
-
-  function set_yjsu_yjad_Cookie() {
-    Cookies.set("_yjsu_yjad", "yjsuyjad_value", cookieOptions);
-  }
-
-  function get_yjsu_yjad_Cookie() {
-    alert(td.getYahooCAPICookie());
-  }
-
-  function set_ycl_yjad_Cookie() {
-    Cookies.set("_ycl_yjad", "yclyjad_value", cookieOptions);
-  }
-
-  function get_ycl_yjad_Cookie() {
-    alert(td.getYahooClickID_ycl_yjad_Cookie());
-  }
-
-  function set_yjr_yjad_Cookie() {
-    Cookies.set("_yjr_yjad", "yjryjad_value", cookieOptions);
-  }
-
-  function get_yjr_yjad_Cookie() {
-    alert(td.getYahooClickID_yjr_yjad_Cookie());
-  }
-
-  function set_twclid_Cookie() {
-    Cookies.set("_twclid", "twclid_value", cookieOptions);
-  }
-
-  function get_twclid_Cookie() {
-    alert(td.getXClickIDCookie());
-  }
-
-  function set_lt_cid_Cookie() {
-    Cookies.set("__lt__cid", "ltcid_value", cookieOptions);
-  }
-
-  function get_lt_cid_Cookie() {
-    alert(td.getLineCookie());
-  }
-
   function getElement(id) {
     return document.querySelector(`#${id}`);
   }
@@ -104,50 +18,159 @@
   function addClickEvent(element, handler) {
     element.addEventListener("click", handler);
   }
+  // =======================================================
+
+  // SET PARAMS ============================================
+  function addGoogle_gclid_Param() {
+    addUrlParam("gclid", "gclid_value");
+  }
+
+  function addGoogle_wbraid_Param() {
+    addUrlParam("wbraid", "wbraid_value");
+  }
+
+  function addFacebook_fbclid_Param() {
+    addUrlParam("fbclid", "fbclid_value");
+  }
+
+  function addYahoo_yclid_Param() {
+    addUrlParam("yclid", "yclid_value");
+  }
+
+  function addLine_ldtagcl_Param() {
+    addUrlParam("ldtag_cl", "ldtag_cl_value");
+  }
+
+  function addX_twclid_Param() {
+    addUrlParam("twclid", "twclid_value");
+  }
+
+  function addPinterest_epik_Param() {
+    addUrlParam("epik", "epik_value");
+  }
+
+  function addSnapchat_sccid_Param() {
+    addUrlParam("ScCid", "sccid_value");
+  }
+  // END SET PARAMS ============================================
+
+  // SET COOKIES ============================================
+  function set_ga_Cookie() {
+    Cookies.set("_ga", "ga_value", cookieOptions);
+  }
+
+  function set_gcl_Cookie() {
+    var prefix = getElement("gcl-prefix").value || "_gcl";
+    var name = getElement("gcl-name").value;
+
+    if (!name) {
+      return alert("Input your gcl cookie name");
+    }
+
+    Cookies.set(`${prefix}_${name}`, `${prefix}_${name}`, cookieOptions);
+  }
+
+  function set_fbp_Cookie() {
+    Cookies.set("_fbp", "fbp_value", cookieOptions);
+  }
+
+  function set_fbc_Cookie() {
+    Cookies.set("_fbc", "fbc_value", cookieOptions);
+  }
+
+  function set_shbts_Cookie() {
+    Cookies.set("shbts", "shbts_value", cookieOptions);
+  }
+
+  function set_shbid_Cookie() {
+    Cookies.set("shbid", "shbid_value", cookieOptions);
+  }
+
+  function set_dsuserid_Cookie() {
+    Cookies.set("ds_user_id", "dsuserid_value", cookieOptions);
+  }
+
+  function set_igdid_Cookie() {
+    Cookies.set("ig_did", "igdid_value", cookieOptions);
+  }
+
+  function set_yjsu_yjad_Cookie() {
+    Cookies.set("_yjsu_yjad", "yjsuyjad_value", cookieOptions);
+  }
+
+  function set_ycl_yjad_Cookie() {
+    Cookies.set("_ycl_yjad", "yclyjad_value", cookieOptions);
+  }
+
+  function set_yjr_yjad_Cookie() {
+    Cookies.set("_yjr_yjad", "yjryjad_value", cookieOptions);
+  }
+
+  function set_ltcid_Cookie() {
+    Cookies.set("__lt__cid", "__lt__cid_value", cookieOptions);
+  }
+
+  function set_ltsid_Cookie() {
+    Cookies.set("__lt__sid", "__lt__sid_value", cookieOptions);
+  }
+
+  function set_twclid_Cookie() {
+    Cookies.set("_twclid", "twclid_value", cookieOptions);
+  }
+
+  function set_epik_Cookie() {
+    Cookies.set("_epik", "epik_value", cookieOptions);
+  }
+
+  function set_ttp_Cookie() {
+    Cookies.set("_ttp", "ttp_value", cookieOptions);
+  }
+
+  function set_mktotrk_Cookie() {
+    Cookies.set("_mkto_trk", "mktotrk_value", cookieOptions);
+  }
+
+  function set_utagmain_Cookie() {
+    Cookies.set("utag_main", "utagmain_value", cookieOptions);
+  }
+  // END SET COOKIES ============================================
 
   function setup() {
     // add para actions
-    addClickEvent(getElement("para_gclid"), addGoogleClidParam);
-    addClickEvent(getElement("para_fbclid"), addFacebookClidParam);
+    addClickEvent(getElement("para_gclid"), addGoogle_gclid_Param);
+    addClickEvent(getElement("para_wbraid"), addGoogle_gclid_Param);
+    addClickEvent(getElement("para_fbclid"), addFacebook_fbclid_Param);
     addClickEvent(getElement("para_yclid"), addYahoo_yclid_Param);
-    addClickEvent(getElement("para_yjryjad"), addYahoo_yjr_yjad_Param);
+    addClickEvent(getElement("para_ldtagcl"), addLine_ldtagcl_Param);
+    addClickEvent(getElement("para_twclid"), addX_twclid_Param);
+    addClickEvent(getElement("para_epik"), addPinterest_epik_Param);
+    addClickEvent(getElement("para_sccid"), addSnapchat_sccid_Param);
 
     // add cookie actions
     addClickEvent(getElement("c_ga"), set_ga_Cookie);
-    addClickEvent(getElement("c_gclaw"), set_gcl_aw_Cookie);
+    addClickEvent(getElement("c_gmp"), set_gcl_Cookie);
+
     addClickEvent(getElement("c_fbp"), set_fbp_Cookie);
     addClickEvent(getElement("c_fbc"), set_fbc_Cookie);
-    addClickEvent(getElement("c_yjsuyjad"), set_yjsu_yjad_Cookie);
+
+    addClickEvent(getElement("c_shbts"), set_shbts_Cookie);
+    addClickEvent(getElement("c_shbid"), set_shbid_Cookie);
+    addClickEvent(getElement("c_dsuserid"), set_dsuserid_Cookie);
+    addClickEvent(getElement("c_igdid"), set_igdid_Cookie);
+
     addClickEvent(getElement("c_yclyjad"), set_ycl_yjad_Cookie);
     addClickEvent(getElement("c_yjryjad"), set_yjr_yjad_Cookie);
+    addClickEvent(getElement("c_yjsuyjad"), set_yjsu_yjad_Cookie);
+
+    addClickEvent(getElement("c_ltcid"), set_ltcid_Cookie);
+    addClickEvent(getElement("c_ltsid"), set_ltsid_Cookie);
+
     addClickEvent(getElement("c_twclid"), set_twclid_Cookie);
-    addClickEvent(getElement("c_ltcid"), set_lt_cid_Cookie);
 
-    addClickEvent(getElement("get_ga"), get_ga_Cookie);
-    addClickEvent(getElement("get_gclaw"), get_gcl_aw_Cookie);
-    addClickEvent(getElement("get_fbp"), get_fbp_Cookie);
-    addClickEvent(getElement("get_fbc"), get_fbc_Cookie);
-    addClickEvent(getElement("get_ycapi"), get_yjsu_yjad_Cookie);
-    addClickEvent(getElement("get_yclyjad"), get_ycl_yjad_Cookie);
-    addClickEvent(getElement("get_yjryjad"), get_yjr_yjad_Cookie);
-    addClickEvent(getElement("get_twclid"), get_twclid_Cookie);
-    addClickEvent(getElement("get_ltcid"), get_lt_cid_Cookie);
-
-    addClickEvent(getElement("get_p_gclid"), function () {
-      alert(td.getGoogleClickIDParam());
-    });
-
-    addClickEvent(getElement("get_p_fbclid"), function () {
-      alert(td.getFacebookClickIDParam());
-    });
-
-    addClickEvent(getElement("get_p_yclid"), function () {
-      alert(td.getYahooClickID_yclid_Param());
-    });
-
-    addClickEvent(getElement("get_p_yjryjad"), function () {
-      alert(td.getYahooClickID_yjr_yjad_Param());
-    });
+    addClickEvent(getElement("c_epik"), set_epik_Cookie);
+    addClickEvent(getElement("c_ttp"), set_ttp_Cookie);
+    addClickEvent(getElement("c_mktotrk"), set_mktotrk_Cookie);
+    addClickEvent(getElement("c_utagmain"), set_utagmain_Cookie);
   }
 
   setup();
