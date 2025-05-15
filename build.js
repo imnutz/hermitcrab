317063092c01b338e8d03f20e6234fee8bf76438
Son Do <son.do@treasure-data.com>
2025-05-15 09:30:40 +0700

    rt p13n

--- build.js
+++ build.js
@@ -14,6 +14,8 @@ function replaceTdInfo(
   apiKey,
   segmentToken,
   profileToken,
+  rtEndpoint,
+  wp13nToken
 ) {
   database = database || fakeInfo;
   host = host || fakeInfo;
@@ -29,6 +31,32 @@ function replaceTdInfo(
       .replace(/writeKey:.*('|")/g, `writeKey: '${apiKey}'`)
       .replace(/audienceToken:.*\[.*\]/g, `audienceToken: ['${segmentToken}']`)
       .replace(/token:.*\[.*\]/g, `token: '${profileToken}'`);
+
+    fs.writeFileSync(fileName, replacedData, "utf-8");
+  });
+}
+
+function replaceRTPersonalizationInfo(
+  path,
+  database,
+  host,
+  apiKey,
+  rtEndpoint,
+  wp13nToken
+) {
+  database = database || fakeInfo;
+  host = host || fakeInfo;
+  apiKey = apiKey || fakeInfo;
+
+  var files = glob.sync(path);
+  files.forEach((fileName) => {
+    var data = fs.readFileSync(fileName, "utf-8");
+    var replacedData = data
+      .replace(/database:.*('|")/g, `database: '${database}'`)
+      .replace(/host:.*('|")/g, `host: '${host}'`)
+      .replace(/writeKey:.*('|")/g, `writeKey: '${apiKey}'`)
+      .replace(/endpoint:.*\[.*\]/g, `endpoint: ${rtEndpoint}`)
+      .replace(/token:.*\[.*\]/g, `token: '${wp13nToken}'`);
 
     fs.writeFileSync(fileName, replacedData, "utf-8");
   });
@@ -91,6 +119,18 @@ replaceTdInfo(
   tdHostAp03,
   apiKeyAp03,
   segmentTokenAp03,
+);
+
+
+let { awsDevApiKey, awsDevWp13nToken, awsDevP13nEndpoint } = process.env;
+replaceRTPersonalizationInfo(
+  "./rt_personalization.html",
+  database,
+  tdHostAp03,
+  awsDevApiKey,
+  segmentTokenAp03,
+  awsDevP13nEndpoint,
+  awsDevWp13nToken
 );
 
 fastApiKey();
