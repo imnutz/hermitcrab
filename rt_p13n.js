2e45e8a9296fc25fe1e7e3e7943042a3102b7a2b
Son Do <son.do@treasure-data.com>
2025-06-26 10:00:21 +0700

    more buttons

--- rt_p13n.js
+++ rt_p13n.js
@@ -5,11 +5,13 @@
   var tbInput = document.querySelector('#tb');
   var payloadInput = document.querySelector('#payload');
   var requestBtn = document.querySelector('#request');
+  var addToCartBtn = document.querySelector('#add_to_cart');
+  var buyNowBtn = document.querySelector('#buy_now');
+  var addToWishlist = document.querySelector('#add_to_wishlist');
+  var subscribeBtn = document.querySelector('#subscribe');
   var display = document.querySelector('#display');
 
-  requestBtn.addEventListener('click', function(evt) {
-    evt.preventDefault();
-
+  var sendRequest = function(payload) {
     if (!tokenInput.value) return alert('Need token');
     if (!dbInput.value) return alert('Need database');
     if (!tbInput.value) return alert('Need table');
@@ -18,7 +20,7 @@
     var token = tokenInput.value;
     var db = dbInput.value;
     var table = tbInput.value;
-    var payloadText = payloadInput.value || "{}";
+    var payloadText = payload || payloadInput.value || "{}";
     var payloadJson = {}
 
     try {
@@ -42,5 +44,41 @@
         console.error(e);
       }
     );
+
+  }
+
+  requestBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    sendRequest()
+  });
+  addToCartBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    sendRequest(JSON.stringify({
+      "td_path": "auto_1",
+      "td_browser": "Chrome",
+      "email": "auto_1@gmail.com" 
+    }));
+  });
+  buyNowBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    sendRequest(JSON.stringify({
+      "td_path": "auto_2",
+      "td_language": "English",
+      "email": "auto_2@gmail.com"
+    }));
+  });
+  addToWishlist.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    sendRequest(JSON.stringify({
+      "td_path": "auto_3",
+      "email": "ak_20250603_1@gmail.com"
+    }));
+  });
+  subscribeBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    sendRequest(JSON.stringify({
+      "td_path": "auto_3",
+      "email": "ak_20250603_1@gmail.com"
+    }));
   });
 }()
